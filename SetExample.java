0d373c264b19663d8664abb2ddd7f47618d8c8fc
Chetna15496 <59188342+Chetna15496@users.noreply.github.com>
2019-12-30 17:16:58 +0530

    Set Example

---
+++ SetExample.java
@@ -0,0 +1,50 @@
+package com.deloitte;
+import java.util.*;
+public class SetExample {
+
+	public static void main(String[] args) {
+		/*Set items=new HashSet();
+		items.add("Mani");
+		items.add(25);
+		items.add("sita");
+		items.add(true);
+		items.add(25);
+		items.add(30);
+		for(Object item:items)
+		{
+			System.out.println(item);
+		}*/
+		/*Set <Integer>items=new TreeSet<>();
+		items.add(20);
+		items.add(25);
+		items.add(23);
+		items.add(2);
+		items.add(25);
+		items.add(30);
+		items.add(7);
+		items.add(21);
+		items.add(3);
+		
+		for(int item:items)
+		{
+			System.out.println(item);
+		}*/
+		Set <Integer>items=new HashSet<>();
+		items.add(20);
+		items.add(25);
+		items.add(23);
+		items.add(2);
+		items.add(25);
+		items.add(30);
+		items.add(7);
+		items.add(21);
+		items.add(3);
+		
+		for(int item:items)
+		{
+			System.out.println(item);
+		}
+
+	}
+
+}
