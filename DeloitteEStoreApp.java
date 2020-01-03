edf891f82a1c3344c464510454a4742f9dc70245
Chetna15496 <59188342+Chetna15496@users.noreply.github.com>
2020-01-03 11:01:26 +0530

    Deloitte E store
    JDBC

--- DeloitteEStoreApp.java
+++ DeloitteEStoreApp.java
@@ -1,14 +1,20 @@
 package com.deloitte.estore.application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 import com.deloitte.estore.model.Product;
 import com.deloitte.estore.service.ProductService;
 import com.deloitte.estore.service.ProductServiceImpl;
 
 public class DeloitteEStoreApp {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
 	ProductService service=new ProductServiceImpl();
-	Product p=new Product(1011,"Mobile",40000);
+	Product p=new Product(1018,"Desktop",40000);
 	try {
 		if(service.addProduct(p))
 		{
@@ -24,9 +30,63 @@ public class DeloitteEStoreApp {
 		{
 			e.printStackTrace();
 		}
+	     p.setProductId(1015);
+	try {
+		if(service.deleteProduct(p))
+		{
+			System.out.println("Product deleted..");
+			
+		}
+		else
+		{
+			System.out.println("Not deleted");
+		}
+	}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	 p.setProductName("Projector");
+	 p.setProductId(1017);
 	
+		try {
+			if(service.updateProduct(p))
+			{
+				System.out.println("Product updated..");
+				
+			}
+			else
+			{
+				System.out.println("Not updated");
+			}
+		}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		try
+		{
+			System.out.println("Enter the product id: ");
+			int pid= sc.nextInt();
+			Product rslt= service.getProductById(pid);
+			System.out.println("Product Id: "+rslt.getProductId()+"\nProduct name: "+rslt.getProductName()+"\nProduct price: "+rslt.getPrice());
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	 try {
+		 List<Product>pList= new ArrayList<>();
+		 pList=service.getAllProducts();
+		// System.out.print(pList);
+		 pList.forEach(System.out::println);
+	 }
+	 catch(Exception e)
+	 {
+		 e.printStackTrace();
+	 
 	
-
+	 }
 	}
 
 }
--- ProductRepoImpl.java
+++ ProductRepoImpl.java
@@ -1,9 +1,13 @@
 package com.deloitte.estore.repo;
 
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.deloitte.estore.model.Product;
@@ -26,27 +30,60 @@ public class ProductRepoImpl implements ProductRepo {
 
 
 	@Override
-	public boolean deleteProduct(Product product) {
-		// TODO Auto-generated method stub
+	public boolean deleteProduct(Product product) throws Exception {
+		Connection con = getDbConnection();
+		PreparedStatement ps= con.prepareStatement("delete from product where product_id =?");
+		ps.setInt(1,product.getProductId());
+		if(ps.executeUpdate()>0)
+			return true;
 		return false;
+	
 	}
 
 	@Override
-	public boolean updateProduct(Product product) {
-		// TODO Auto-generated method stub
+	public boolean updateProduct(Product product) throws Exception {
+		Connection con = getDbConnection();
+		PreparedStatement ps= con.prepareStatement("update product set product_name=? where product_id=?");
+		ps.setString(1,product.getProductName());
+		ps.setInt(2,product.getProductId());
+		if(ps.executeUpdate()>0)
+			return true;
 		return false;
+	
 	}
 
 	@Override
-	public Product getProductById(int productId) {
-		// TODO Auto-generated method stub
+	public Product getProductById(int productId) throws Exception {
+		Connection con = getDbConnection();
+		Statement st= con.createStatement();
+	    ResultSet rs=st.executeQuery("select * from product where product_id="+productId);
+		//PreparedStatement ps= con.prepareStatement("select * from product where product_id=?");
+		Product p=new Product();
+		if(rs.next()) {
+			p.setProductId(rs.getInt(1));
+			p.setProductName(rs.getString(2));
+			p.setPrice(rs.getFloat(3));
+			return p;}
 		return null;
-	}
+		}
+		
+	
+	
 
 	@Override
-	public List<Product> getAllProducts() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Product> getAllProducts() throws Exception {
+		Connection con = getDbConnection();
+		PreparedStatement ps=con.prepareStatement("select * from product");
+	    ResultSet rs=ps.executeQuery();
+	    List<Product> products=new ArrayList<>();
+	    while(rs.next()) {
+	    	Product p= new Product();
+	    	p.setProductId(rs.getInt("product_id"));
+	    	p.setProductName(rs.getString("product_name"));
+	    	p.setPrice(rs.getFloat("price"));
+	    	products.add(p);
+	    }
+	    return products;
 	}
 
 	@Override
